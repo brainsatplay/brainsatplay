d4a6338d7bc5770a0137e1428b2f189807b6f02d
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-03-12 15:13:41 -0800

    moar

--- src/js/appv2.js
+++ src/js/appv2.js
@@ -32,12 +32,155 @@ Frontend Execution
 
 */
 
+import {eeg32, eegmath} from './utils/eeg32'
+import {Biquad, makeNotchFilter, makeBandpassFilter, DCBlocker} from '../utils/signal_analysis/BiquadFilters'
+
+
+class biquadChannelFilterer {
+    constructor(channel="A0",sps=512, filtering=true) {
+        this.channel=channel; this.idx = 0; this.sps = sps;
+        this.filtering=filtering;
+        this.bplower = 3; this.bpupper = 45;
+
+        State.data.filtered[this.channel] = [];//Add placeholder to state
+
+        this.notch50 = [
+                    makeNotchFilter(50,sps,1)
+                ];
+        this.notch60 = [
+                    makeNotchFilter(60,sps,1)
+                ];
+        this.lp1 = [
+                    new Biquad('lowpass', 50, sps),
+                    new Biquad('lowpass', 50, sps),
+                    new Biquad('lowpass', 50, sps),
+                    new Biquad('lowpass', 50, sps)
+                ];
+        this.bp1 = [
+                    makeBandpassFilter(this.bplower,this.bpupper,sps,1),
+                    makeBandpassFilter(this.bplower,this.bpupper,sps,1),
+                    makeBandpassFilter(this.bplower,this.bpupper,sps,1),
+                    makeBandpassFilter(this.bplower,this.bpupper,sps,1)
+                ];
+        this.dcb = new DCBlocker(0.995);
+    }
+
+    reset(sps=this.sps) {
+        this.notch50 = makeNotchFilter(50,sps,1);
+        this.notch60 = makeNotchFilter(60,sps,1);
+        this.lp1 = [
+                    new Biquad('lowpass', 50, sps),
+                    new Biquad('lowpass', 50, sps),
+                    new Biquad('lowpass', 50, sps),
+                    new Biquad('lowpass', 50, sps)
+                ];
+        this.bp1 = [
+                    makeBandpassFilter(this.bplower,this.bpupper,sps,9.75)
+                ];
+        this.dcb = new DCBlocker(0.995);
+    }
+
+    setBandpass(bplower=this.bplower,bpupper=this.bpupper) {
+        this.bplower=bplower; this.bpupper = bpupper;
+        this.bp1 = [
+            makeBandpassFilter(bplower,bpupper,sps),
+            makeBandpassFilter(bplower,bpupper,sps),
+            makeBandpassFilter(bplower,bpupper,sps),
+            makeBandpassFilter(bplower,bpupper,sps)
+        ];
+    }
+
+    apply(idx=this.lastidx+1) {
+        let out=EEG.data[this.channel][idx]; 
+        if(this.filtering === true) {
+            if(State.data.sma4 === true) {
+                if(State.data.counter >= 4) { //Apply a 4-sample moving average
+                    out = (State.data.filtered[this.channel][State.data.filtered[this.channel].length-3] + State.data.filtered[this.channel][State.data.filtered[this.channel].length-2] + State.data.filtered[this.channel][State.data.filtered[this.channel].length-1] + out)*.25;
+                }
+                else if(EEG.data.counter >= 4){
+                    //console.log(State.data.counter, State.data.filtered[this.channel].length)
+                    out = (EEG.data[this.channel][EEG.data.counter-4] + EEG.data[this.channel][EEG.data.counter-3] + EEG.data[this.channel][EEG.data.counter-2] + out)*.25;
+                }
+            }
+            if(State.data.dcblocker === true) { //Apply a DC blocking filter
+                out = this.dcb.applyFilter(out);
+            }
+            if(State.data.notch50 === true) { //Apply a 50hz notch filter
+                this.notch50.forEach((f,i) => {
+                    out = f.applyFilter(out);
+                });
+            }
+            if(State.data.notch60 === true) { //Apply a 60hz notch filter
+                this.notch60.forEach((f,i) => {
+                    out = f.applyFilter(out);
+                });
+            } 
+            if(State.data.lowpass50 === true) { //Apply 4 50Hz lowpass filters
+                this.lp1.forEach((f,i) => {
+                    out = f.applyFilter(out);
+                });
+            }
+            if(State.data.bandpass === true) { //Apply 4 Bandpass filters
+                this.bp1.forEach((f,i) => {
+                    out = f.applyFilter(out);
+                });
+            }
+            if(State.data.uVScaling === true){
+                out = out*EEG.uVperStep;
+            }
+        }
+        this.lastidx=idx;
+        //console.log(this.channel, out)
+        return out;
+    }
+}
+
+
+class deviceStream {
+	constructor(device="FreeEEG32_2",location="local",useFilters="true",pipeTo=null,auth={}) {
+		this.deviceObj = null;
+		this.filters = [];
+		if(device === "FreeEEG32_2") {
+			let defaultTags = [
+				{ch: 4, tag: "Fp2", viewing: true},
+				{ch: 24, tag: "Fp1", viewing: true},
+				{ch: 8, tag: "other", viewing: true}
+			];
+			
+			this.deviceObj = new eeg32(
+				(newLinesint) => {
+
+				},
+				()=>{},
+				()=>{}
+			);
+			if(useFilters === true) {
+				defaultTags.forEach((row,i) => {
+					if(row.tag !== 'other') {
+						State.data.filterers.push(new biquadChannelFilterer("A"+row.ch,this.deviceObj.sps,true));
+					}
+					else { 
+						State.data.filterers.push(new biquadChannelFilterer("A"+row.ch,this.deviceObj.sps,false)); 
+					}
+				});
+			}
+			
+		}
+		else if(device === "FreeEEG32_19") {
+
+		}
+		else if(device === "muse") {
+
+		}
+	}
+}
+
 
 class dataAtlas {
     constructor(channelTags=[{ch: 0, tag: null},{ch: 1, tag: null}],name="atlas",use10_20=true,useCoherence=true) {
         this.name = name;
         this.tags = channelTags;
-        this.data = {shared:{},eeg:[],coherence:[],heg:{}};
+        this.data = {shared:{},eeg:[],coherence:[],heg:[]};
         if(use10_20 === true) {
             this.data.eeg = this.gen10_20Atlas();
         }
@@ -331,221 +474,3 @@ class dataAtlas {
 }
 
 
-export class eegmath {
-	constructor() {
-
-	}
-
-	//----------------------------------------------------------------
-	//-------------------- Static Functions --------------------------
-	//----------------------------------------------------------------
-
-	//Generate sinewave, you can add a noise frequency in too. Array length will be Math.ceil(fs*nSec)
-	static genSineWave(freq=20,peakAmp=1,nSec=1,fs=512,freq2=0,peakAmp2=1){
-		var sineWave = [];
-		var t = [];
-		var increment = 1/fs; //x-axis time increment based on sample rate
-		for (var ti = 0; ti < nSec; ti+=increment){
-			var amplitude = Math.sin(2*Math.PI*freq*ti)*peakAmp;
-			amplitude += Math.sin(2*Math.PI*freq2*ti)*peakAmp2; //Add interference
-			sineWave.push(amplitude);
-			t.push(ti);
-		}
-		return [t,sineWave]; // [[times],[amplitudes]]
-	}
-
-	static mean(arr){
-		var sum = arr.reduce((prev,curr)=> curr += prev);
-		return sum / arr.length;
-	}
-
-	static variance(arr1) { //1D input arrays of length n
-		var mean1 = this.mean(arr1);
-		var vari = [];
-		for(var i = 0; i < arr1.length; i++){
-			vari.push((arr1[i] - mean1)/(arr1.length-1));
-		}
-		return vari;
-	}
-
-	static transpose(mat){
-		return mat[0].map((_, colIndex) => mat.map(row => row[colIndex]));
-	}
-
-	//Matrix multiplication from: https://stackoverflow.com/questions/27205018/multiply-2-matrices-in-javascript
-	static matmul(a, b) {
-		var aNumRows = a.length, aNumCols = a[0].length,
-			bNumRows = b.length, bNumCols = b[0].length,
-			m = new Array(aNumRows);  // initialize array of rows
-		for (var r = 0; r < aNumRows; ++r) {
-		  m[r] = new Array(bNumCols); // initialize the current row
-		  for (var c = 0; c < bNumCols; ++c) {
-			m[r][c] = 0;             // initialize the current cell
-			for (var i = 0; i < aNumCols; ++i) {
-			  m[r][c] += a[r][i] * b[i][c];
-			}
-		  }
-		}
-		return m;
-	  }
-
-	//2D matrix covariance (e.g. for lists of signals). Pretty fast!!!
-	static cov2d(mat) { //[[x,y,z,w],[x,y,z,w],...] input list of vectors of the same length
-		//Get variance of rows and columns
-		//console.time("cov2d");
-		var mattransposed = this.transpose(mat);
-		//console.log(mattransposed)
-		var matproducts = [];
-
-		var rowmeans = [];
-		var colmeans = [];
-
-		mat.forEach((row, idx) => {
-			rowmeans.push(this.mean(row));
-		});
-
-		mattransposed.forEach((col,idx) => {
-			colmeans.push(this.mean(col));
-		});
-
-		mat.forEach((row,idx) => {
-			matproducts.push([]);
-			for(var col = 0; col < row.length; col++){
-				matproducts[idx].push((mat[idx][col]-rowmeans[idx])*(mat[idx][col]-colmeans[col])/(row.length - 1));
-			}
-		});
-
-		/*
-			mat[y][x] = (x - rowAvg)*(x - colAvg) / (mat[y].length - 1);
-		*/
-
-		console.log(matproducts);
-		//Transpose matrix
-		var matproductstransposed = this.transpose(matproducts);
-
-		//Matrix multiplication, stolen from: https://stackoverflow.com/questions/27205018/multiply-2-matrices-in-javascript
-		var aNumRows = matproducts.length, aNumCols = matproducts[0].length,
-			bNumRows = matproductstransposed.length, bNumCols = matproductstransposed[0].length,
-			m = new Array(aNumRows);  // initialize array of rows
-		for (var r = 0; r < aNumRows; ++r) {
-		  m[r] = new Array(bNumCols); // initialize the current row
-		  for (var c = 0; c < bNumCols; ++c) {
-			m[r][c] = 0;             // initialize the current cell
-			for (var i = 0; i < aNumCols; ++i) {
-			  m[r][c] += matproducts[r][i] * matproductstransposed[i][c] / (mat[0].length - 1); //divide by row length - 1
-			}
-		  }
-		}
-		//console.timeEnd("cov2d");
-		return m; //Covariance matrix
-	}
-
-	//Covariance between two 1D arrays
-	static cov1d(arr1,arr2) {
-		return this.cov2d([arr1,arr2]);
-	}
-
-	//Simple cross correlation.
-	static crosscorrelation(arr1,arr2) {
-
-		//console.time("crosscorrelation");
-		var arr2buf = [...arr2,...Array(arr2.length).fill(0)];
-		var mean1 = this.mean(arr1);
-		var mean2 = this.mean(arr2);
-
-		//Estimators
-		var arr1Est = arr1.reduce((sum,item) => sum += Math.pow(item-mean1,2));
-		arr1Est = Math.sqrt(arr1Est);
-		var arr2Est = arr2.reduce((sum,item) => sum += Math.pow(item-mean1,2));
-		arr2Est = Math.sqrt(arr2Est);
-
-		var _arrEstsMul = 1/(arr1Est * arr2Est);
-		var correlations = new Array(arr1.length).fill(0);
-
-		for(var delay = 0; delay < arr1.length; delay++) {
-			var r = arr1.reduce((sum,item,i) => sum += (item - mean1)*(arr2buf[delay+i]-mean2));
-			correlations[delay] = r*_arrEstsMul;
-		}
-
-		//console.timeEnd("crosscorrelation");
-		return correlations;
-	}
-
-	//Simple autocorrelation. Better method for long series: FFT[x1] .* FFT[x2]
-	static autocorrelation(arr1) {
-		var delaybuf = [...arr1,...Array(arr1.length).fill(0)];
-		var mean1 = this.mean(arr1);
-
-		//Estimators
-		var arr1Est = arr1.reduce((sum,item) => sum += Math.pow(item-mean1,2));
-		arr1Est = Math.sqrt(arr1Est);
-
-		var _arr1estsqrd = 1/(arr1Est * arr1Est);
-		var correlations = new Array(arr1.length).fill(0);
-
-		for(var delay = 0; delay < arr1.length; delay++) {
-			var r = arr1.reduce((sum,item,i) => sum += (item - mean1)*(delaybuf[delay+i]-mean1));
-			correlations[delay] = r*_arr1estsqrd;
-		}
-
-		return correlations;
-	}
-
-	//Compute correlograms of the given array of arrays (of equal length). Input array of equal length arrays of latest raw data (use dat = eeg32instance.getTaggedRawData())
-	static correlograms(dat) {//Coherence network math for data pushed to the atlas
-		var correlograms = []; //auto and cross correlations for each channel
-		dat.forEach((row1,i) => {
-			dat.forEach((row2,j) => {
-				if(j >= i) {
-					correlograms.push(eegmath.crosscorrelation(row1,row2));
-				}
-			})
-		});
-		return correlograms; //Output ordered like (tag1:tag1, tag1:tag2 ... tag2:tag2, tag2:tag3 ... tagn:tagn) where autocorrelograms are also included
-	}
-
-
-	//Input data and averaging window, output array of moving averages (should be same size as input array, initial values not fully averaged due to window)
-	static sma(arr, window) {
-		var smaArr = []; //console.log(arr);
-		for(var i = 0; i < arr.length; i++) {
-			if((i == 0)) {
-				smaArr.push(arr[0]);
-			}
-			else if(i < window) { //average partial window (prevents delays on screen)
-				var arrslice = arr.slice(0,i+1);
-				smaArr.push(arrslice.reduce((previous,current) => current += previous ) / (i+1));
-			}
-			else { //average windows
-				var arrslice = arr.slice(i-window,i);
-				smaArr.push(arrslice.reduce((previous,current) => current += previous) / window);
-			}
-		}
-		//console.log(temp);
-		return smaArr;
-	}
-
-	//Linear interpolation from https://stackoverflow.com/questions/26941168/javascript-interpolate-an-array-of-numbers. Input array and number of samples to fit the data to
-	interpolateArray(data, fitCount) {
-
-		var norm = this.canvas.height/data.length;
-
-		var linearInterpolate = function (before, after, atPoint) {
-			return (before + (after - before) * atPoint)*norm;
-		};
-
-		var newData = new Array();
-		var springFactor = new Number((data.length - 1) / (fitCount - 1));
-		newData[0] = data[0]; // for new allocation
-		for ( var i = 1; i < fitCount - 1; i++) {
-			var tmp = i * springFactor;
-			var before = new Number(Math.floor(tmp)).toFixed();
-			var after = new Number(Math.ceil(tmp)).toFixed();
-			var atPoint = tmp - before;
-			newData[i] = linearInterpolate(data[before], data[after], atPoint);
-		}
-		newData[fitCount - 1] = data[data.length - 1]; // for new allocation
-		return newData;
-	};
-
-}
