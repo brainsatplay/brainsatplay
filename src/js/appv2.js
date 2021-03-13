b05f7502e245bd01ae2a31fe7f4c8949a2c6b73d
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-03-13 01:25:54 -0800

    Update appv2.js

--- src/js/appv2.js
+++ src/js/appv2.js
@@ -241,6 +241,7 @@ class biquadChannelFilterer {
                 this.bp1.forEach((f,i) => {
                     out = f.applyFilter(out);
                 });
+				out = out*this.bp1.length;
             }
             if(this.useScaling === true){
                 out = out*this.scalingFactor;
