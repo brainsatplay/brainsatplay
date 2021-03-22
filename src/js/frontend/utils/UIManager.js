a9654ddb99d854a6f0a9fe2d50f5a98374c8644f
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-03-21 23:06:15 -0700

    Update UIManager.js

--- src/js/frontend/utils/UIManager.js
+++ src/js/frontend/utils/UIManager.js
@@ -163,7 +163,7 @@ export class UIManager {
         })
     }
 
-    responsiveUIUpdate(nodes=this.applets, topoffset=140) {
+    responsiveUIUpdate(nodes=this.applets, topoffset=110) {
         //console.log(nodes);
         nodes.forEach((node,i) => {
             //console.log(node)
