3eb0af4a87f21d6252a7d5eab86f2886cf56f829
Garrett <gflynn@usc.edu>
2021-04-10 09:11:18 -0500

    Galaxy from Three.js Journey

---
+++ src/js/applets/galaxy/GalaxyApplet.js
@@ -0,0 +1,317 @@
+import {brainsatplay} from '../../brainsatplay'
+import {DOMFragment} from '../../frontend/utils/DOMFragment'
+
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import vertexShader from './shaders/galaxy/vertex.glsl'
+import fragmentShader from './shaders/galaxy/fragment.glsl'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
+import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass'
+import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+import { gsap } from 'gsap'
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
+import dummyTexture from "./img/dummyTexture.jpeg"
+
+// import * as p5 from 'p5'
+// console.log(p5.noise)
+
+//Example Applet for integrating with the UI Manager
+export class GalaxyApplet {
+
+    static devices = ['eeg'] //,heg
+
+    constructor(
+        parent=document.body,
+        bci=new brainsatplay(),
+        settings=[]
+    ) {
+    
+        //-------Keep these------- 
+        this.parentNode = parent;
+        this.settings = settings;
+        this.bci = bci; //Reference to the brainsatplay session to access data and subscribe
+        this.AppletHTML = null;
+        //------------------------
+
+        this.props = { //Changes to this can be used to auto-update the HTML and track important UI values 
+            id: String(Math.floor(Math.random()*1000000)), //Keep random ID
+            //Add whatever else
+        };
+
+        //etc..
+
+    }
+
+    //---------------------------------
+    //---Required template functions---
+    //---------------------------------
+
+    //Initalize the app with the DOMFragment component for HTML rendering/logic to be used by the UI manager. Customize the app however otherwise.
+    init() {
+
+        //HTML render function, can also just be a plain template string, add the random ID to named divs so they don't cause conflicts with other UI elements
+        let HTMLtemplate = (props=this.props) => { 
+            return `
+            <div id='${props.id}' class="galaxy-wrapper" style='height:${props.height}; width:${props.width};'>
+                <div id="galaxy-renderer-container"><canvas class="galaxy-webgl"></canvas></div>
+                <div class="galaxy-gui-container"></div>
+            </div>
+            `;  
+        }
+
+        //HTML UI logic setup. e.g. buttons, animations, xhr, etc.
+        let setupHTML = (props=this.props) => {
+            document.getElementById(props.id);
+        }
+
+        this.AppletHTML = new DOMFragment( // Fast HTML rendering container object
+            HTMLtemplate,       //Define the html template string or function with properties
+            this.parentNode,    //Define where to append to (use the parentNode)
+            this.props,         //Reference to the HTML render properties (optional)
+            setupHTML,          //The setup functions for buttons and other onclick/onchange/etc functions which won't work inline in the template string
+            undefined,          //Can have an onchange function fire when properties change
+            "NEVER"             //Changes to props or the template string will automatically rerender the html template if "NEVER" is changed to "FRAMERATE" or another value, otherwise the UI manager handles resizing and reinits when new apps are added/destroyed
+        );  
+
+        if(this.settings.length > 0) { this.configure(this.settings); } //You can give the app initialization settings if you want via an array.
+
+
+
+/**
+ * Galaxy
+ */
+const loadingManager = new THREE.LoadingManager(
+    // Loaded
+    () => {
+        gsap.delayedCall(0.1,() => 
+        {
+            canvas.style.opacity = '1'
+            resizeGalaxy()
+        })
+    }, 
+    // Progress
+    (itemURL, itemsLoaded, itemsTotal) => {
+        console.log(itemsLoaded/itemsTotal)
+    }
+)
+const textureLoader = new THREE.TextureLoader(loadingManager)
+textureLoader.load(dummyTexture)
+
+
+ // Debug
+ const gui = new GUI()
+
+ // Canvas
+ const galaxyContainer = document.getElementById(this.props.id)
+ const canvas = document.querySelector('canvas.galaxy-webgl')
+ 
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Galaxy
+ */
+const parameters = {}
+parameters.count = 200000
+parameters.size = 0.005
+parameters.radius = 5
+parameters.branches = 3
+parameters.spin = 1
+parameters.randomness = 0.2
+parameters.randomnessPower = 3
+parameters.insideColor = '#ff6030'
+parameters.outsideColor = '#1b3984'
+
+let geometry = null
+let material = null
+let points = null
+
+const generateGalaxy = () =>
+{
+    if(points !== null)
+    {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+
+    /**
+     * Geometry
+     */
+    geometry = new THREE.BufferGeometry()
+
+    const positions = new Float32Array(parameters.count * 3)
+    const randomness = new Float32Array(parameters.count * 3)
+    const colors = new Float32Array(parameters.count * 3)
+    const scales = new Float32Array(parameters.count * 1)
+
+    const insideColor = new THREE.Color(parameters.insideColor)
+    const outsideColor = new THREE.Color(parameters.outsideColor)
+
+    for(let i = 0; i < parameters.count; i++)
+    {
+        const i3 = i * 3
+
+        // Position
+        const radius = Math.random() * parameters.radius
+
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+
+        positions[i3    ] = Math.cos(branchAngle) * radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = Math.sin(branchAngle) * radius
+    
+        randomness[i3    ] = randomX
+        randomness[i3 + 1] = randomY
+        randomness[i3 + 2] = randomZ
+
+        // Color
+        const mixedColor = insideColor.clone()
+        mixedColor.lerp(outsideColor, radius / parameters.radius)
+
+        colors[i3    ] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
+
+        // Scale
+        scales[i] = Math.random()
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    geometry.setAttribute('aRandomness', new THREE.BufferAttribute(randomness, 3))
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+    geometry.setAttribute('aScale', new THREE.BufferAttribute(scales, 1))
+
+    /**
+     * Material
+     */
+    material = new THREE.ShaderMaterial({
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true,
+        uniforms:
+        {
+            uTime: { value: 0 },
+            uSize: { value: 30 * renderer.getPixelRatio() }
+        },    
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    })
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+}
+
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+
+let resizeGalaxy = () => {
+    // Update camera
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(galaxyContainer.clientWidth, galaxyContainer.clientHeight)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+}
+
+window.addEventListener('resize', () =>
+{
+    resizeGalaxy()
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+camera.position.x = 3
+camera.position.y = 3
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(galaxyContainer.clientWidth, galaxyContainer.clientHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Generate the first galaxy
+ */
+generateGalaxy()
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update material
+    let neurofeedback = (0.5 + 0.5*Math.sin(Date.now()/1000))
+    material.uniforms.uTime.value += 0.001 + 0.01*neurofeedback
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
+    }
+
+    //Delete all event listeners and loops here and delete the HTML block
+    deinit() {
+        this.AppletHTML.deleteNode();
+        //Be sure to unsubscribe from state if using it and remove any extra event listeners
+    }
+
+    //Responsive UI update, for resizing and responding to new connections detected by the UI manager
+    responsive() {
+        // this.resizeGalaxy()
+    }
+
+    configure(settings=[]) { //For configuring from the address bar or saved settings. Expects an array of arguments [a,b,c] to do whatever with
+        settings.forEach((cmd,i) => {
+            //if(cmd === 'x'){//doSomething;}
+        });
+    }
+
+    //--------------------------------------------
+    //--Add anything else for internal use below--
+    //--------------------------------------------
+
+    //doSomething(){}
+} 
