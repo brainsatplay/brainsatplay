481505afd2ba978456cbc3b9abdb42bcf88f27e0
Garrett <gflynn@usc.edu>
2021-04-10 15:42:01 -0500

    Changed branch to "deploy"

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 # #!/bin/bash
 # build the build for public url 
 npm run build
-# make sure to add dist 
-git add build -f
+# make sure to add deploy 
+git add deploy -f
 
 # commit the GH pages changes 
-git commit -m "gh-pages commit"
+git commit -m "deploy commit"
 # push to subtree remote 
-git subtree push --prefix build origin build
+git subtree push --prefix build origin deploy
 
 # remove build directory
-sudo rm -rf build
+sudo rm -rf deploy
 
