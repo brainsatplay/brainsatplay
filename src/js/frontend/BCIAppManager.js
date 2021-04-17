d2fb593cbf2458df2d5a5f10c18e6b61ec9f3297
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-04-16 19:35:26 -0700

    proper command

--- src/js/frontend/BCIAppManager.js
+++ src/js/frontend/BCIAppManager.js
@@ -399,8 +399,7 @@ export class BCIAppManager {
         hasharr.shift();
         let appletConfigs = [];
         hasharr.forEach((hash,i) => {
-            let rep = hash.replaceAll('%22','"');
-            rep = hash.replaceAll('%20',' ');
+            let rep = decodeURIComponent(hash);
             rep = rep.replaceAll("'",'"'); //replace single quotes with double quotes
             let cfg;
             if(rep.indexOf('{') > -1) //parse if its an object
