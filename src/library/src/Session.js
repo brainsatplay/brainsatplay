84b11e447ba8ce73a0886325626162eb936d7652
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-04-30 17:28:06 -0700

    Update Session.js

--- src/library/src/Session.js
+++ src/library/src/Session.js
@@ -168,12 +168,14 @@ export class Session {
 				)
 			);
 
-			if(streamParams[0]) { this.addStreamParams(streamParams); this.beginStream();}
-
+			
 			let i = this.devices.length-1;
 
 			this.devices[i].onconnect = () => {
 				this.info.nDevices++;
+				if(streamParams[0]) { this.addStreamParams(streamParams); this.beginStream();}
+				//Device info accessible from state
+				this.state.addToState("device"+(i),this.devices[i].info);
 				onconnect();
 				this.onconnected();
 			}
@@ -182,9 +184,8 @@ export class Session {
 				ondisconnect();
 				this.ondisconnected();
 				if(Array.isArray(this.devices[i].info.analysis) && this.devices[i].info.analysis.length > 0) {
-					this.devices[i].info.analyzing = false; //cancel analysis loop
+					this.devices[i].atlas.analyzing = false; //cancel analysis loop
 				}
-				this.devices[i].info.streaming = false; //cancel stream loop
 				this.devices.splice(i,1);			
 				this.info.nDevices--;
 			}
@@ -192,9 +193,7 @@ export class Session {
 			this.devices[i].init();
 
 			if(this.devices.length === 1) this.atlas = this.devices[0].atlas; //change over from dummy atlas
-			//Device info accessible from state
-			this.state.addToState("device"+(i),this.devices[i].info);
-			
+
 			this.devices[i].connect();
 	}
 
