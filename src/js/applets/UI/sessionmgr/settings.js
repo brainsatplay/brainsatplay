bffa99e34ba0d8ee702f3b054eccbaf59f0473c1
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-05-02 18:03:25 -0700

    quick fix

---
+++ src/js/applets/UI/sessionmgr/settings.js
@@ -0,0 +1,12 @@
+import featureImg from './../../../../assets/features/placeholder.png'
+
+export const settings = {
+    "name": "Example",
+    "devices": ["EEG","HEG"],
+    "author": "Joshua Brewster",
+    "description": "Example",
+    "categories": ["train"],
+    "module": "SessionManagerApplet",
+    "image": featureImg,
+		"instructions":"Coming soon..."
+}
