b54072cca66d086874c4a6da9c7f0e4f0fd13017
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-05-04 11:46:39 -0700

    tweakig

--- src/js/applets/UI/sessionmgr/SessionManagerApplet.js
+++ src/js/applets/UI/sessionmgr/SessionManagerApplet.js
@@ -6,14 +6,6 @@ import * as BrowserFS from 'browserfs'
 const fs = BrowserFS.BFSRequire('fs');
 const BFSBuffer = BrowserFS.BFSRequire('buffer').Buffer;
 
-if(!document.getElementById('googleapi')) {
-document.head.insertAdjacentHTML('beforeend',`
-<script id='googleapi' async defer src="https://apis.google.com/js/api.js"
-      onload="this.onload=function(){};handleClientLoad()"
-      onreadystatechange="if (this.readyState === 'complete') this.onload()">
-</script>
-`);
-}
 
 //Session reviewer! Yay!
 export class SessionManagerApplet {
@@ -69,7 +61,7 @@ export class SessionManagerApplet {
                 <p>Google Sheets API Quickstart</p>
 
                 <!--Add buttons to initiate auth sequence and sign out-->
-                <button id="authorize_button" style="display: none;">Authorize</button>
+                <button id="authorize_button">Authorize</button>
                 <button id="signout_button" style="display: none;">Sign Out</button>
 
                 <pre id="content" style="white-space: pre-wrap;"></pre>
@@ -80,118 +72,7 @@ export class SessionManagerApplet {
         //HTML UI logic setup. e.g. buttons, animations, xhr, etc.
         let setupHTML = (props=this.props) => {
 
-            // Array of API discovery doc URLs for APIs used by the quickstart
-            var DISCOVERY_DOCS = [
-                "https://sheets.googleapis.com/$discovery/rest?version=v4",
-                "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
-            ];
 
-            var authorizeButton = document.getElementById('authorize_button');
-            var signoutButton = document.getElementById('signout_button');
-
-            /**
-             *  On load, called to load the auth2 library and API client library.
-             */
-            function handleClientLoad() {
-                gapi.load('client:auth2', initClient);
-            }
-
-            /**
-             *  Initializes the API client library and sets up sign-in state
-             *  listeners.
-             */
-            function initClient() {
-                gapi.client.init({
-                    apiKey: 'AIzaSyDkUs-ofe1TPDftg4_T5wcA8y7qp03f6nU',
-                    clientId: '354011508571-521lgm8ulo8nl6bevis1n94nlekf44ge.apps.googleusercontent.com',
-                    discoveryDocs: DISCOVERY_DOCS,
-                    scope: `https://www.googleapis.com/auth/drive.appdata 
-                    https://www.googleapis.com/auth/drive.file 
-                    https://www.googleapis.com/auth/drive.install 
-                    https://www.googleapis.com/auth/docs 
-                    https://www.googleapis.com/auth/drive 
-                    https://www.googleapis.com/auth/drive.metadata 
-                    https://www.googleapis.com/auth/drive.scripts 
-                    https://www.googleapis.com/auth/drive.activity 
-                    https://www.googleapis.com/auth/spreadsheets`
-                }).then(function () {
-                    // Listen for sign-in state changes.
-                    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-                    // Handle the initial sign-in state.
-                    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-                    authorizeButton.onclick = handleAuthClick;
-                    signoutButton.onclick = handleSignoutClick;
-                }, function(error) {
-                    appendPre(JSON.stringify(error, null, 2));
-                });
-            }
-
-            /**
-             *  Called when the signed in status changes, to update the UI
-             *  appropriately. After a sign-in, the API is called.
-             */
-            function updateSigninStatus(isSignedIn) {
-                if (isSignedIn) {
-                    authorizeButton.style.display = 'none';
-                    signoutButton.style.display = 'block';
-                    listMajors();
-                } else {
-                    authorizeButton.style.display = 'block';
-                    signoutButton.style.display = 'none';
-                }
-            }
-
-            /**
-             *  Sign in the user upon button click.
-             */
-            function handleAuthClick(event) {
-                gapi.auth2.getAuthInstance().signIn();
-            }
-
-            /**
-             *  Sign out the user upon button click.
-             */
-            function handleSignoutClick(event) {
-                gapi.auth2.getAuthInstance().signOut();
-            }
-
-            /**
-             * Append a pre element to the body containing the given message
-             * as its text node. Used to display the results of the API call.
-             *
-             * @param {string} message Text to be placed in pre element.
-             */
-            function appendPre(message) {
-                var pre = document.getElementById('content');
-                var textContent = document.createTextNode(message + '\n');
-                pre.appendChild(textContent);
-            }
-
-            /**
-             * Print the names and majors of students in a sample spreadsheet:
-             * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-             */
-            function listMajors() {
-                gapi.client.sheets.spreadsheets.values.get({
-                    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-                    range: 'Class Data!A2:E',
-                }).then(function(response) {
-                    var range = response.result;
-                    if (range.values.length > 0) {
-                    appendPre('Name, Major:');
-                    for (i = 0; i < range.values.length; i++) {
-                        var row = range.values[i];
-                        // Print columns A and E, which correspond to indices 0 and 4.
-                        appendPre(row[0] + ', ' + row[4]);
-                    }
-                    } else {
-                    appendPre('No data found.');
-                    }
-                }, function(response) {
-                    appendPre('Error: ' + response.result.error.message);
-                });
-            }
         }
 
         this.AppletHTML = new DOMFragment( // Fast HTML rendering container object
--- src/library/src/ui/login.js
+++ src/library/src/ui/login.js
@@ -1,7 +1,7 @@
 
 // Auth
 import * as Realm from "realm-web";
-import googleOneTap from 'google-one-tap';
+// import googleOneTap from 'google-one-tap';
 
 // let mongodb = {
 //     collection: "profiles",
@@ -18,7 +18,63 @@ const redirectUri = `${window.location.origin}/`;
 const credentials = Realm.Credentials.google(redirectUri);
 export const LoginWithGoogle = async () => {
     return app.logIn(credentials).then(async user => {
-        await user.refreshCustomData()
+        await user.refreshCustomData();
+
+        function handleClientLoad() {
+            window.gapi.load('client:auth2', initClient);
+        }
+
+        if(!document.getElementById('googleapi')) {
+            document.head.insertAdjacentHTML('beforeend',`
+                <script id='googleapi' async defer src="https://apis.google.com/js/api.js">
+                </script>
+            `);
+            document.getElementById('googleapi').onload = () => {this.onload=function(){};handleClientLoad();}
+            document.getElementById('googleapi').onreadystatechange = () => {if (this.readyState === 'complete') this.onload();}
+        }
+
+        function updateSigninStatus(isSignedIn) {
+            if (isSignedIn) {
+              console.log("Signed in with Google, Drive, Docs, and Sheets available.")
+            } else {
+                console.log("Signed out of Google")
+            }
+          }
+
+        // Array of API discovery doc URLs for APIs used by the quickstart
+        var DISCOVERY_DOCS = [
+            "https://sheets.googleapis.com/$discovery/rest?version=v4",
+            "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
+        ];
+
+
+        function initClient() {
+            window.gapi.client.init({
+                apiKey: 'AIzaSyDkUs-ofe1TPDftg4_T5wcA8y7qp03f6nU',
+                clientId: '354011508571-521lgm8ulo8nl6bevis1n94nlekf44ge.apps.googleusercontent.com',
+                discoveryDocs: DISCOVERY_DOCS,
+                scope: `https://www.googleapis.com/auth/drive.appdata 
+                https://www.googleapis.com/auth/drive.file 
+                https://www.googleapis.com/auth/drive.install 
+                https://www.googleapis.com/auth/docs 
+                https://www.googleapis.com/auth/drive 
+                https://www.googleapis.com/auth/drive.metadata 
+                https://www.googleapis.com/auth/drive.scripts 
+                https://www.googleapis.com/auth/drive.activity 
+                https://www.googleapis.com/auth/spreadsheets`
+            }).then(function () {
+                // Listen for sign-in state changes.
+                window.gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+                // Handle the initial sign-in state.
+                updateSigninStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get());
+                authorizeButton.onclick = handleAuthClick;
+                signoutButton.onclick = handleSignoutClick;
+            }, function(error) {
+                console.log(error);//appendPre(JSON.stringify(error, null, 2));
+            });
+        }
+
         return user
     }).catch(e => {console.error(e)})
 }
