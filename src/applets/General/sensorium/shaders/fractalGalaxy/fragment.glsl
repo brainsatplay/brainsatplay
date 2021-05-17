5e006de151a2cde25af30797b5882a1e9531a075
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-05-16 22:05:45 -0700

    Update fragment.glsl

--- src/applets/General/sensorium/shaders/fractalGalaxy/fragment.glsl
+++ src/applets/General/sensorium/shaders/fractalGalaxy/fragment.glsl
@@ -23,7 +23,7 @@ float field(in vec3 p) {
 	float tw = 0.;
 	for (int i = 0; i < 32; ++i) {
 		float mag = dot(p, p);
-		p = abs(p) / mag + vec3(-.5, -.4+(iAudio[200]*0.00001), -1.5);
+		p = abs(p) / mag + vec3(-.5+(iAudio[100]*0.00001), -.4+(iAudio[200]*0.00001), -1.5);
 		float w = exp(-float(i) / 7.);
 		if (iHEG < 0.0){
 			accum += w - exp(-strength  *pow(abs(mag - prev), 2.3));
