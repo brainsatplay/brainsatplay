81b4c578922a919a3edf8a26b980755bec6e2ec0
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-05-27 23:50:42 -0700

    fix 8ch prototype timestamps

--- src/libraries/js/src/devices/8ch_prototype/8chPlugin.js
+++ src/libraries/js/src/devices/8ch_prototype/8chPlugin.js
@@ -49,8 +49,8 @@ export class Prototype8Plugin {
                                 let coord;
                                 if(o.tag !== null) { coord = this.atlas.getEEGDataByTag(o.tag); } 
                                 else { coord = this.atlas.getEEGDataByChannel(o.ch); }
-                                coord.count ++;
-                                coord.times.push(this.device.data.ms[ct]);
+                                coord.count++;
+                                coord.times.push(this.device.data.ms[ct-1]);
                                 coord.filtered.push(latestFiltered);
                                 coord.raw.push(latest);
                             }
@@ -59,7 +59,7 @@ export class Prototype8Plugin {
                             if(info.useAtlas === true) {
                                 let coord = this.atlas.getEEGDataByChannel(o.ch); 
                                 coord.count += newLinesInt;
-                                coord.times.push(this.device.data.ms[ct]);
+                                coord.times.push(this.device.data.ms[ct-1]);
                                 coord.raw.push(latest);
                             }
                         }
