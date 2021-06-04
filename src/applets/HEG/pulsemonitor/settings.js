8a23fd3991bc8afaf7556e7326a5a36f58b36900
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-06-04 15:42:46 -0700

    edits

--- src/applets/HEG/pulsemonitor/settings.js
+++ src/applets/HEG/pulsemonitor/settings.js
@@ -11,5 +11,25 @@ export const settings = {
     "image":  featureImg,
 		"instructions":`
     <p>View HEG data in real time, see your raw blood oxygen ratio and heart rate variability over time.</p>
+    <h3>Easy breathing exercises:</h3>
+    <h4>Diaphragmatic Vagal Breathing (increase score and HRV):</h4>
+    <p>
+      Take a slow deep breath over 5 seconds, then breathe out slowly for 5 seconds. Keep a steady (coherent) rate. <br>
+      Do not try to strain yourself or hold your breath in.<br>
+      The breathing should feel be a natural but slow ~6 breaths per minute rhythm that pushes your belly out and your diaphragm down. <br>
+    </p>
+    <h4>Relaxation breathing (reduces score)</h4>
+    <p>
+      With eyes closed: <br>
+      Breathe out slowly over 7 seconds till you empty your lungs, then breathe back in for 3 seconds. <br>
+      Focus on a point in the middle of your forehead and slowly move it down your body, or simply count your breaths. This keeps your mind occupied. <br>
+    </p>
+    <h4>Jacobson's Progressive Muscular Relaxation Breathing</h4>
+    <p>
+      With eyes closed: <br>
+      Another exercise is to breathe similarly to the first exercise but even slower while also tensing different muscle groups on the inhale, then relaxing them on the exhale. <br>
+      Start in either your upper or lower body and work your way up/down through all of your muscle groups. <br>
+      Tense up only once every two to three breathing cycles to give your body time to recover in-between.
+    </p>
     `
 }
