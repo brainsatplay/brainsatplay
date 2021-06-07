3fc28e7757fe5f8e19c9c70bca7dd13dce01c06c
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-06-07 13:15:02 -0700

    updated descriptions

--- src/applets/HEG/breathtrainer/settings.js
+++ src/applets/HEG/breathtrainer/settings.js
@@ -12,5 +12,34 @@ export const settings = {
     "display": {
       production:false
     },
-		"instructions":"Coming soon..." // Extended instructions on how to use your applet
+		"instructions":`
+    <p>Follow the sine waves with your breathing pattern! See below for different explanations.</p>
+    <h3>Easy breathing exercises:</h3>
+    <h4>Deep Diaphragmatic Breathing (increase score and HRV):</h4>
+    <p>
+      Take a slow deep breath over 5 seconds, then breathe out slowly for 5 seconds. Keep a steady (coherent) rate. <br>
+      Do not try to strain yourself or hold your breath in.<br>
+      The breathing should feel be a natural but slow ~6 breaths per minute rhythm that pushes your belly out and your diaphragm down. <br> 
+      This will stimulate a vagus nerve response as well which causes brief but significant shifts in heart rate and blood pressure. <br>
+    </p>
+    <h4>Relaxation breathing (reduces score)</h4>
+    <p>
+      With eyes closed: <br>
+      Breathe out slowly over 7 seconds till you empty your lungs, then breathe back in for 3 seconds. <br>
+      Focus on a point in the middle of your forehead and slowly move it down your body, or simply count your breaths. This keeps your mind occupied. <br>
+    </p>
+    <h4>Jacobson's Progressive Muscular Relaxation Breathing</h4>
+    <p>
+      With eyes closed: <br>
+      Another exercise is to breathe similarly to the first exercise but even slower while also tensing different muscle groups on the inhale, then relaxing them on the exhale. <br>
+      Start in either your upper or lower body and work your way up/down through all of your muscle groups. <br>
+      Tense up only once every two to three breathing cycles to give your body time to recover in-between.
+    </p>
+    <h4>Wim Hof Method</h4>
+    <p>
+      Take 30-40 rapid deep breaths, about 1 breath per second, and deep enough to extend and contract your diaphragm. <br>
+      Then immediately ease into a very slow 5 seconds in 5 seconds out deep breathing pattern for about 3 to 4 breaths. <br>
+      You can keep repeating this to intensify the effect.
+    </p>
+    `
   }
