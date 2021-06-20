b7261054bd6f172f1e17a057af5af8d42138e602
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-06-20 14:28:40 -0700

    Update cytonPlugin.js

--- src/libraries/js/src/devices/cyton/cytonPlugin.js
+++ src/libraries/js/src/devices/cyton/cytonPlugin.js
@@ -135,9 +135,10 @@ export class cytonPlugin {
                 // this.filters[this.filters.length-1].notch60.pop();
                 // this.filters[this.filters.length-1].notch50.pop();
                 this.filters[this.filters.length-1].notch60.push(
-                    this.filters[this.filters.length-1].makeNotchFilter(60,sps,1));
+                    this.filters[this.filters.length-1].makeNotchFilter(60,this.info.sps,1)
+                    );
                 this.filters[this.filters.length-1].notch50.push(
-                    this.filters[this.filters.length-1].makeNotchFilter(50,sps,1)
+                    this.filters[this.filters.length-1].makeNotchFilter(50,this.info.sps,1)
                     );
                 this.filters[this.filters.length-1].useBp1 = true;
                 this.filters[this.filters.length-1].useScaling = true;
