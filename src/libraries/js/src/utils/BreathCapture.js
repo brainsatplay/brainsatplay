f8af44216d7e65011c4d38d4a2eb044aa0c53d89
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-06-26 14:07:56 -0700

    Update BreathCapture.js

--- src/libraries/js/src/utils/BreathCapture.js
+++ src/libraries/js/src/utils/BreathCapture.js
@@ -15,17 +15,17 @@ Capture.calibrate();
 //Outputs captured in this object as arrays
 //More fine grained data are in the class
 Capture.output = {
-            belowThreshold: false,              //Are the detected breathing peaks below the mean threshold (too quiet?) //bool
-            isHolding: false,                   //Is the user between an in and out breath?     //bool
-            inVolumes: this.inPeakVolumes,      //summed fft volume of in-breath                //float array
-            outVolumes: this.outPeakVolumes,    //summed fft volume of out-breath               //float array
-            inTimes: this.inPeakTimes,          //timestamps of in-breaths                      //unix ms timestamp array
-            outTimes: this.outPeakTimes,        //timestamps of out-breaths                     //unix ms timestamp array
-            inToOutTimes: this.inToOutTimes,    //timeframe between out- and in-breaths         //ms int array
-            fastTimes: this.fastPeakTimes,      //timestamps of the fast sma peaks              //unix ms timestamp array
-            fastRate: this.fastPeakDt,          //For fast breathing look for coherent breaths  //ms int array
-            breathRate: this.breathingRate,     //look for coherent breaths                     //ms int array
-            brv: this.breathingRateVariability  //Lower is better                               //ms int array
+    belowThreshold: false,              //Are the detected breathing peaks below the mean threshold (too quiet?) //bool
+    isHolding: false,                   //Is the user between an in and out breath?     //bool
+    inVolumes: this.inPeakVolumes,      //summed fft volume of in-breath                //float array
+    outVolumes: this.outPeakVolumes,    //summed fft volume of out-breath               //float array
+    inTimes: this.inPeakTimes,          //timestamps of in-breaths                      //unix ms timestamp array
+    outTimes: this.outPeakTimes,        //timestamps of out-breaths                     //unix ms timestamp array
+    inToOutTimes: this.inToOutTimes,    //timeframe between out- and in-breaths         //ms int array
+    fastTimes: this.fastPeakTimes,      //timestamps of the fast sma peaks              //unix ms timestamp array
+    fastRate: this.fastPeakDt,          //For fast breathing look for coherent breaths  //ms int array
+    breathRate: this.breathingRate,     //look for coherent breaths                     //ms int array
+    brv: this.breathingRateVariability  //Lower is better                               //ms int array
 };
 
 */
