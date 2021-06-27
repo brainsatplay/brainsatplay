04e26ff4ce0a5f1a73211e61aa57d38bf423b04e
Garrett <gflynn@usc.edu>
2021-06-27 14:43:12 -0500

    Added feature for Breath Garden

--- src/libraries/js/src/plugins/utilities/Shader.js
+++ src/libraries/js/src/plugins/utilities/Shader.js
@@ -9,16 +9,17 @@ export class Shader{
 
         this.props = {
             id: String(Math.floor(Math.random() * 1000000)),
+            uniforms: {}
         }
 
         this.ports = {
             default: {
                 default: this.params.glsl ?? '', 
-                meta: {label: this.label, uniforms: this.params.uniforms},
+                meta: {label: this.label, uniforms: this.props.uniforms},
                 input: {type: null},
                 output: {type: 'glsl'},
                 onUpdate: () => {
-                    return [{data: this.params.glsl, meta: {label: this.label, uniforms: this.params.uniforms}}]
+                    return [{data: this.params.glsl, meta: {label: this.label, uniforms: this.props.uniforms}}]
                 }
             },
             set: {
@@ -26,44 +27,50 @@ export class Shader{
                 output: {type: null},
                 onUpdate: (userData) => {
                     let u = userData[0]
-                    this.params.glsl = u.data
-                    this._setDynamicPorts(this.params.glsl)
-                    this.session.graph.runSafe(this,'default',[{data:true}])
+                    if (typeof u.data === 'string'){
+                        this.params.glsl = u.data
+                        this._setDynamicPorts(this.params.glsl)
+                        this.session.graph.runSafe(this,'default',[{data:true}])
+                    }
                 }
             }
         }
     }
 
     init = () => {
+        if (this.params.uniforms) this.props.uniforms = this.params.uniforms
+        delete this.params.uniforms
         this.session.graph.runSafe(this,'set',[{data:this.params.glsl}])
     }
 
     deinit = () => {}
 
     _setDynamicPorts = (glsl) => {
-
         // Get Uniforms
         var re = /uniform\s+([^\s]+)\s+([^;]+);/g;
         let result = [...glsl.matchAll(re)]
-        this.props.uniforms = []
 
-        result.forEach(a => {
-            let name = a[2]
-            let type = a[1]
-
-            // Set Port
-            this.ports[name] = {
-                input: {type},
-                default: this.params.uniforms[name]?.value,
-                output: {type},
-                onUpdate: (userData) => {
-                    this.params.uniforms[name].value = userData[0].data
-                    return [{data: this.ports[name].output.value, meta: {label: `${this.label}_${name}`}}]
-                }
-            }
-
-            // Set Param
-            this.params[name] = this.params.uniforms[name]?.value
+        result.forEach((match) => {
+            this._setPort(match)
         })
+    }
+
+    _setPort = (match) => {
+        let name = match[2]
+        let type = match[1]
+
+        // Set Port
+        this.ports[name] = {
+            input: {type},
+            default: this.props.uniforms[name]?.value,
+            output: {type},
+            onUpdate: (userData) => {
+                this.props.uniforms[name].value = userData[0].data
+                return [{data: this.ports[name].output.value, meta: {label: `${this.label}_${name}`}}]
+            }
+        }
+
+        // Set Param
+        this.params[name] = this.props.uniforms[name]?.value
     }
 }
