7dbc6d77b09c306c26efbe61cecbcb9c310d587c
Garrett <gflynn@usc.edu>
2021-07-03 15:40:49 -0500

    Hot-linked the GPLv3 license in the about page

--- src/platform/js/frontend/AboutPage.js
+++ src/platform/js/frontend/AboutPage.js
@@ -21,7 +21,7 @@ export class AboutPage{
                 <p>For more information about our organization, please visit <a href="https://brainsatplay.com">https://brainsatplay.com</a>. And, if you're interested in building applications with Brains@Play, make sure to check out our <a href="https://github.com/brainsatplay/brainsatplay">GitHub repository</a> and <a href="https://docs.brainsatplay.com">documentation</a> site.</p>
 
                 <div style="font-size: 75%;">
-                    <p><strong>Note:</strong> This version of The Brains@Play Platform is an alpha release. While there's much work ahead, we encourage you to try things out and get in touch with your suggestions for improvement. </p> <p>This will be a community effort—and we're grateful to have you on our team.</p>
+                    <p><strong>Note:</strong> This is an alpha version of The Brains@Play Platform released without warrenty under the <a href="https://choosealicense.com/licenses/gpl-3.0/">GPLv3 license</a>. While there's much work ahead, we encourage you to try things out and get in touch with your suggestions for improvement.</p><p>This will be a community effort—and we're grateful to have you on our team.</p>
                 </div>
                 </div>
         </div>
