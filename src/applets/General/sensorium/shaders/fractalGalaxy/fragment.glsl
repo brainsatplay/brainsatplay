74f1edaad909a22912ee147e9da6b532bbfdc65d
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-07-02 17:20:42 -0700

    Update fragment.glsl

--- src/applets/General/sensorium/shaders/fractalGalaxy/fragment.glsl
+++ src/applets/General/sensorium/shaders/fractalGalaxy/fragment.glsl
@@ -21,7 +21,7 @@ float field(in vec3 p,float s) {
 	float accum = s/4.;
 	float prev = 0.;
 	float tw = 0.;
-	for (int i = 0; i < 22; ++i) {
+	for (int i = 0; i < 14; ++i) {
 		float mag = dot(p, p);
 		p = abs(p) / mag + vec3(-.5, -.4, -1.5);
 		float w = exp(-float(i) / 7.);
@@ -38,7 +38,7 @@ float field2(in vec3 p, float s) {
 	float accum = s/4.;
 	float prev = 0.;
 	float tw = 0.;
-	for (int i = 0; i < 16; ++i) {
+	for (int i = 0; i < 14; ++i) {
 		float mag = dot(p, p);
 		p = abs(p) / mag + vec3(-.5, -.4, -1.5);
 		float w = exp(-float(i) / 7.);
@@ -61,7 +61,7 @@ vec3 nrand3( vec2 co )
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     vec2 uv = 2. * fragCoord.xy / iResolution.xy - 1.;
 	vec2 uvs = uv * iResolution.xy / max(iResolution.x, iResolution.y);
-	vec3 p = vec3(uvs / 4., 0) + vec3(1., -1.3, 0.);
+	vec3 p = vec3(uvs, 0) + vec3(1., -1.3, 0.);
 	p += .2 * vec3(sin(iTime / 16.), sin(iTime / 12.),  sin(iTime / 128.));
 	
 	float freqs[4];
@@ -72,11 +72,11 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
 	freqs[2] = iAudio[125]/255.+iFFT[125]*.1+.15;
 	freqs[3] = iAudio[200]/255.+iFFT[200]*.1+.3;
 
-	float t = field(p,freqs[2]);
+	float t = field(p,freqs[2])*2.;
 	float v = (1. - exp((abs(uv.x) - 1.) * 6.)) * (1. - exp((abs(uv.y) - 1.) * 6.));
 	
     //Second Layer
-	vec3 p2 = vec3(uvs / (4.+sin(iTime*0.11)*0.2+0.2+sin(iTime*0.15)*0.3+0.4), 1.5) + vec3(2., -1.3, -1.);
+	vec3 p2 = vec3(5.*uvs / (4.+sin(iTime*0.11)*0.2+0.2+sin(iTime*0.15)*0.3+0.4), 1.5) + vec3(2., -1.3, -1.);
 	p2 += 0.25 * vec3(sin(iTime / 16.), sin(iTime / 12.),  sin(iTime / 128.));
 	float t2 = field2(p2,freqs[3]);
 	vec4 c2 = mix(.4, 1., v) * vec4(1.3 * t2 * t2 * t2 ,1.8  * t2 * t2 , t2* freqs[0], t2);
@@ -84,13 +84,13 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
 	
 	//Let's add some stars
 	//Thanks to http://glsl.heroku.com/e#6904.0
-	vec2 seed = p.xy * 200.;	
+	vec2 seed = p.xy * 75.;	
 	seed = floor(seed * iResolution.x);
 	vec3 rnd = nrand3( seed );
 	vec4 starcolor = vec4(pow(rnd.y,40.0));
 	
 	//Second Layer
-	vec2 seed2 = p2.xy * 200.;
+	vec2 seed2 = p2.xy * 50.;
 	seed2 = floor(seed2 * iResolution.x);
 	vec3 rnd2 = nrand3( seed2 );
 	starcolor += vec4(pow(rnd2.y,40.0));
