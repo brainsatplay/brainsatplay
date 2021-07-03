ed364a57a32fb0d8dc2636942ec1df9da0b7192c
Garrett <gflynn@usc.edu>
2021-07-03 17:47:26 -0500

    Update index.js

--- src/libraries/js/src/plugins/outputs/index.js
+++ src/libraries/js/src/plugins/outputs/index.js
@@ -1,9 +1,7 @@
 import {Debug} from './Debug'
 import {Cursor} from './Cursor'
 import {Buzz} from './Buzz'
-import {Canvas} from '../graphics/Canvas'
-import {Scene} from '../graphics/Scene'
 import {UI} from './UI'
 import {Plot} from './Plot'
 
-export {Debug,Cursor,Buzz,Canvas,Scene,UI,Plot}
+export {Debug,Cursor,Buzz,UI,Plot}
