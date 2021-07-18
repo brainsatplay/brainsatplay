64eb76a64628b62164864d8bc85ee0f077fad0cb
Garrett <gflynn@usc.edu>
2021-07-17 21:02:58 -0500

    Blink debugger online

--- src/libraries/js/src/plugins/models/Blink.js
+++ src/libraries/js/src/plugins/models/Blink.js
@@ -137,7 +137,7 @@ export class Blink{
                 {  
                     forceRun: true,
                     forceUpdate: true,
-                    data: (ctx) => {
+                    data: {active: true, function: (ctx) => {
                         if (this.props.looping){
                             if (this.params.debug){
                                 this._drawSignal(ctx)
@@ -146,7 +146,7 @@ export class Blink{
                                 this.props.container.style.pointerEvents = 'none'
                             }
                         }
-                    }
+                    }}
                 }
             ])
         }
