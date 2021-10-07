da69d78bc1a457d8aeef6e66da3310ee71fae6b9
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-10-07 00:04:50 -0700

    Update index.js

--- src/libraries/js/src/plugins/utilities/index.js
+++ src/libraries/js/src/plugins/utilities/index.js
@@ -8,7 +8,7 @@ import {Storage} from './Storage'
 import {dynamicImport} from '../../utils/general/importUtils'
 
 //the link is based on where the import utils are 
-const move = async (label, session, params=[]) => {let plugin = await dynamicImport('../../plugins/utilities/Move.js'); console.log(plugin);}; //the url may need to be set based on which script you are trying to import it from
+const move = async (label, session, params=[]) => {let plugin = await dynamicImport('../../plugins/utilities/Move.js'); console.log(plugin); console.log(plugin.Move);}; //the url may need to be set based on which script you are trying to import it from
 
 move();
 
