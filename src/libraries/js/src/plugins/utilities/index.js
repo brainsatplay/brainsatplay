33c96a428e6b7aceb1c792eeb5a179ca0711137f
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-10-06 23:53:24 -0700

    test dynamic import plugin

--- src/libraries/js/src/plugins/utilities/index.js
+++ src/libraries/js/src/plugins/utilities/index.js
@@ -5,6 +5,10 @@ import {MapArray} from './MapArray'
 import {Thread} from './Thread'
 import {Storage} from './Storage'
 
+import {dynamicImport} from '../../utils/general/importUtils'
+
+const move = (label, session, params=[]) => {let plugin = dynamicImport('./Move'); return plugin(label, session, params);}
+
 export {
     Move,
     Scheduler,
