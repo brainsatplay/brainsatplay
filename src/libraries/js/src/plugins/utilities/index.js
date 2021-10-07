a7dff600a17cd36d242297053b7177d30c29f5ff
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-10-07 00:06:56 -0700

    Update index.js

--- src/libraries/js/src/plugins/utilities/index.js
+++ src/libraries/js/src/plugins/utilities/index.js
@@ -10,7 +10,7 @@ import {dynamicImport} from '../../utils/general/importUtils'
 //the link is based on where the import utils are 
 const move = async (label, session, params=[]) => {let plugin = await dynamicImport('../../plugins/utilities/Move.js'); console.log(plugin); console.log(plugin.Move);}; //the url may need to be set based on which script you are trying to import it from
 
-move();
+move(); //do something like this instead of importing all of the plugins, just like how we do applets
 
 export {
     Move,
