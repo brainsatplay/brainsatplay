135ae3cc705c0e44fec47e43fecde035c26f2486
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-04-17 12:15:37 -0700

    Update Process2.ts

--- src/graph/Process2.ts
+++ src/graph/Process2.ts
@@ -4,6 +4,28 @@
 // or propagate to children/parents with utility calls that get added to the objects
 //Joshua Brewster and Garrett Flynn AGPLv3.0
 
+//properties input on GraphNode or addNode, or for children
+export type GraphNodeProperties = {
+    tag?:string, //generated if not specified, or use to get another node by tag instead of generating a new one
+    operator?:(
+        input:any, //input, e.g. output from another node
+        node:GraphNode|string,  //'this' node
+        origin?:GraphNode|string, //origin node
+        cmd?:string    //e.g. 'loop' or 'animate' will be defined if the operator is running on the loop or animate routines, needed something. Can define more commands but you might as well use an object in input for that. 
+    )=>any|AsyncGeneratorFunction, //Operator to handle I/O on this node. Returned inputs can propagate according to below settings
+    forward?:boolean, //pass output to child nodes
+    backward?:boolean, //pass output to parent node
+    children?:string|GraphNodeProperties|GraphNode|(GraphNodeProperties|GraphNode|string)[], //child node(s), can be tags of other nodes, properties objects like this, or graphnodes, or null
+    parent?:GraphNode|undefined, //parent graph node
+    delay?:false|number, //ms delay to fire the node
+    repeat?:false|number, // set repeat as an integer to repeat the input n times
+    recursive?:false|number, //or set recursive with an integer to pass the output back in as the next input n times
+    animate?:boolean, //true or false
+    loop?:false|number, //milliseconds or false
+    [key:string]:any //add whatever variables and utilities
+}; //can specify properties of the element which can be subscribed to for changes.
+
+
 
 /*
 
@@ -59,27 +81,6 @@ console.log('promise returned:',res);
 each node in the tree becomes a GraphNode object
 
 */
-
-//properties input on GraphNode or addNode, or for children
-export type GraphNodeProperties = {
-    tag?:string, //generated if not specified, or use to get another node by tag instead of generating a new one
-    operator?:(
-        input:any, //input, e.g. output from another node
-        node:GraphNode|string,  //'this' node
-        origin?:GraphNode|string, //origin node
-        cmd?:string    //e.g. 'loop' or 'animate' will be defined if the operator is running on the loop or animate routines, needed something. Can define more commands but you might as well use an object in input for that. 
-    )=>any|AsyncGeneratorFunction, //Operator to handle I/O on this node. Returned inputs can propagate according to below settings
-    forward?:boolean, //pass output to child nodes
-    backward?:boolean, //pass output to parent node
-    children?:string|GraphNodeProperties|GraphNode|(GraphNodeProperties|GraphNode|string)[], //child node(s), can be tags of other nodes, properties objects like this, or graphnodes, or null
-    parent?:GraphNode|undefined, //parent graph node
-    delay?:false|number, //ms delay to fire the node
-    repeat?:false|number, // set repeat as an integer to repeat the input n times
-    recursive?:false|number, //or set recursive with an integer to pass the output back in as the next input n times
-    animate?:boolean, //true or false
-    loop?:false|number, //milliseconds or false
-    [key:string]:any //add whatever variables and utilities
-}; //can specify properties of the element which can be subscribed to for changes.
 
 
 
