b2f9cc91e91d218964695b74906caea023a96116
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-04-27 23:44:30 -0700

    organized

--- src/core/bundler.js
+++ src/core/bundler.js
@@ -10,17 +10,10 @@ const {dtsPlugin} = require('esbuild-plugin-d.ts');
 const fs = require('fs');
 
 
+
+//setup
+
 const entryPoints = ['index.ts'];
-
-const INSTALL_GLOBALLY = { //for browser js only right now, it just declares globalThis[key] = import(path) via import * as bundle from 'path'
-  //install bundles as global variables?
-  //globalThis key : imported module (or import * as key from value)
-  brainsatplay: entryPoints[0] //set key values for variables to be accessable from browser script via window/globalThis.key.function() etc.
-};
-
-
-
-
 const outfile = 'dist/index'; 
 //outdir = ['dist/index','dist/index2']; //for multiple files
 
@@ -35,6 +28,13 @@ const sourcemap = false;
 
 const platform = 'browser'; //'node'; //set node for node module compilation, uncomment 
 const external = ['node-fetch']; // [];
+
+const INSTALL_GLOBALLY = {
+  //install bundles with additionally available global variables? Makes it browser scripting-compatible with window variables for bundles.
+  //globalThis key : imported module (or import * as key from value)
+  brainsatplay: entryPoints[0] //set key values for variables to be accessable from browser script via window/globalThis.key.function() etc.
+};
+
 
 
 const loader = {
@@ -52,7 +52,60 @@ const loader = {
   '.json': 'text',
 };
 
+
+
 async function bundle() {
+
+  let temp_files = [...entryPoints];
+  let to_clean = [];
+  if(Object.keys(INSTALL_GLOBALLY).length > 0) {
+
+    entryPoints.forEach((f,i)=>{  
+
+      let ext = f.split('.')[f.split('.').length-1];
+      let subpath = f.substring(0,f.indexOf('.'+ext));
+      //console.log(f,subpath,ext);
+
+      let propname;
+
+      for(const prop in INSTALL_GLOBALLY) { 
+        if(INSTALL_GLOBALLY[prop] === f) {
+          propname = prop;
+        }
+      }
+
+      if(propname) {
+        fs.writeFileSync( //lets make temp files to bundle our bundles (a wrapper) into globalThis properties (still import-friendly in node!)
+          'temp_'+f,
+          `
+          //we can't circularly export a namespace for index.ts so this is the intermediary
+          //import * as bundle from './x' then set INSTALL_GLOBALS[key] = bundle; The only other option is dynamic importing or a bigger bundler with more of these features built in
+          
+          export * from './${subpath}' //still works in esm
+          
+          //this takes all of the re-exported modules in index.ts and contains them in an object
+          import * as bundle from './${subpath}'
+          
+          //webpack? i.e. set the bundled index.ts modules to be globally available? 
+          // You can set many modules and assign more functions etc. to the same globals without error
+          
+          //globals are not declared by default in browser scripts, these files are function scopes!
+        
+          if(typeof globalThis['${propname}'] !== 'undefined') Object.assign(globalThis['${propname}'],bundle); //we can keep assigning the same namespaces more module objects without error!
+          else globalThis['${propname}'] = bundle;
+        
+          ` //we could do more with this with other settings! It just builds this file instead of the original one then deletes the temp file.
+        );
+
+        temp_files[i] = 'temp_'+f;  
+        to_clean.push(temp_files[i]);
+
+      }
+
+    });
+
+  }
+
 
   if(createESMJS) {
     console.time('\n Built .esm.js file(s)')
@@ -75,57 +128,12 @@ async function bundle() {
   if(createBrowserJS) {
     console.time('\n Built browser .js file(s)');
     //our very own esbuild plugin
-    let temp_files = [...entryPoints];
-    let to_clean = [];
-    entryPoints.forEach((f,i)=>{  
-
-      let ext = f.split('.')[f.split('.').length-1];
-      let subpath = f.substring(0,f.indexOf('.'+ext));
-      //console.log(f,subpath,ext);
-
-      let propname;
-
-      for(const prop in INSTALL_GLOBALLY) { 
-        if(INSTALL_GLOBALLY[prop] === f) {
-          propname = prop;
-        }
-      }
-
-      if(propname) {
-        fs.writeFileSync(
-          'temp_'+f,
-          `
-          //we can't circularly export a namespace for index.ts so this is the intermediary
-          //import * as bundle from './x' then set INSTALL_GLOBALS[key] = bundle; The only other option is dynamic importing or a bigger bundler with more of these features built in
-          
-          export * from './${subpath}' //still works in esm
-          
-          //this takes all of the re-exported modules in index.ts and contains them in an object
-          import * as bundle from './${subpath}'
-          
-          //webpack? i.e. set the bundled index.ts modules to be globally available? 
-          // You can set many modules and assign more functions etc. to the same globals without error
-          
-          //globals are not declared by default in browser scripts, these files are function scopes!
-        
-          if(typeof globalThis['${propname}'] !== 'undefined') Object.assign(globalThis['${propname}'],bundle); //we can keep assigning the same namespaces more module objects without error!
-          else globalThis['${propname}'] = bundle;
-        
-          `
-        );
-
-        temp_files[i] = 'temp_'+f;  
-        to_clean.push(temp_files[i]);
-
-      }
-
-    });
-
+    
     
 
 
     await esbuild.build({ //browser-friendly scripting globals
-      entryPoints:temp_files,
+      entryPoints:temp_files, //use the modified files with the globals
       bundle:true,
       logLevel:'error',
       outfile:outfile+'.js', //'.browser.js
@@ -135,7 +143,6 @@ async function bundle() {
       minify:minify,
       loader
     }).then(()=>{
-      to_clean.map(f => fs.unlinkSync(f));
       console.timeEnd('\n Built browser .js file(s)');
     });
       
@@ -185,12 +192,15 @@ async function bundle() {
     });
   }
 
+  //clean temp files we wrote extra code to
+  to_clean.map(f => fs.unlinkSync(f));
 
 
   console.log('esbuild completed!')
   console.timeEnd('esbuild');
 
 }
+
 
 bundle();
 
