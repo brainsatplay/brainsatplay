29cbd6c02a104b857028fab4f07ea35994146eb8
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-05-08 23:14:46 -0700

    push

---
+++ src/build/example/tinybuild.js
@@ -0,0 +1,18 @@
+
+        import { packager, defaultServer } from "tinybuild";
+        let config = {
+            bundler:{
+                entryPoints: ['index.js'], //entry file, relative to this file 
+                outfile: 'dist/index', //exit file
+                //outdir:[] 
+                bundleBrowser: true, //plain js format
+                bundleESM: false, //.esm format
+                bundleTypes: false, //entry point should be a ts or jsx (or other typescript) file
+                bundleHTML: true //can wrap the built outfile (or first file in outdir) automatically and serve it or click and run the file without hosting.
+            },
+            server:defaultServer
+        }
+
+        //bundle and serve
+        packager(config);
+        
