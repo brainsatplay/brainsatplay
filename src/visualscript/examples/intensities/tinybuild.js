1d4d2f61c3231a4e5dba818be2b17ae7392c92ec
Garrett <gflynn@usc.edu>
2022-05-09 09:44:14 -0700

    Mocked up examples

---
+++ src/visualscript/examples/intensities/tinybuild.js
@@ -0,0 +1,16 @@
+import { packager, defaultServer } from "../../../build/tinybuild.js";
+
+let config = {
+    bundler:{
+        entryPoints: ['app.js'],
+        outfile: 'dist/app',
+        bundleBrowser: true, //plain js format
+        bundleESM: false, //.esm format
+        bundleTypes: false, //entry point should be a ts or jsx (or other typescript) file
+        bundleHTML: false
+      },
+    server:defaultServer
+}
+
+//bundle and serve
+packager(config);
