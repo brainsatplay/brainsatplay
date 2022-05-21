1f643a6ce3b02bcc211473306107fa04602fb429
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-05-21 01:08:05 -0700

    updated

--- src/build/tinybuild/esbuild/bundler.js
+++ src/build/tinybuild/esbuild/bundler.js
@@ -6,6 +6,7 @@
 import esbuild from 'esbuild'
 import {dtsPlugin} from 'esbuild-plugin-d.ts'
 import {streamingImportsPlugin} from './streamingImportsPlugin.js'
+import {workerPlugin} from './workerPlugin.js'
 
 import fs from 'fs'
 
@@ -45,10 +46,15 @@ export const defaultBundler = {
   outputs:{ //overwrites main config settings for specific use cases
     node:{ 
       external:[] //externals for node environment builds
+    },
+    //commonjs:{}
+    browser:{
+      plugins:[
+        streamingImportsPlugin,
+        workerPlugin //worker plugin works on esm imported 'worker.js' ending files, or requires in node
+      ] 
     }
     //esm:{}
-    //commonjs:{}
-    //browser:{}
     //iife:{}
   },
   defaultConfig: true //indicates this object is the default config
