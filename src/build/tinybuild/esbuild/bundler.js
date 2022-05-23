c9ab207a58a70d94857744affdf9356501accca2
Garrett <gflynn@usc.edu>
2022-05-23 15:44:04 -0700

    Revert change to fs.rm

--- src/build/tinybuild/esbuild/bundler.js
+++ src/build/tinybuild/esbuild/bundler.js
@@ -286,7 +286,7 @@ export async function bundleBrowser(config) {
     }
     
     //clean temp files we wrote extra code to
-    fs.rmdir(tempDir,{ recursive: true }, () => {})
+    fs.rm(tempDir,{ recursive: true })
 
   }).catch((er)=>{console.error('Exited with error:',er); process.exit();});
 }
