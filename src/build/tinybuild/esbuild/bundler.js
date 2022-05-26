3857ab2177208e9f25892ef2cfa30f02d39494c5
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-05-26 14:44:41 -0700

    Update bundler.js

--- src/build/tinybuild/esbuild/bundler.js
+++ src/build/tinybuild/esbuild/bundler.js
@@ -35,7 +35,7 @@ export const defaultBundler = {
   loader: { //just a bunch of path resolvers, will supply urls if marked 'file', text if marked 'text', and dataurls (blobs) if marked 'dataurl'
     '.html': 'text', //not always necessary but it doesn't hurt
     '.json': 'text','.txt': 'text','.yaml': 'text', '.toml':'text','.md':'text', 
-    '.gitignore':'file',
+    '.gitignore':'file','.wasm': 'file',
     '.xml' : 'file','.xhtml': 'file','.png' : 'file','.PNG' : 'file','.jpg' : 'file','.gif' : 'file','.ico' : 'file',
     '.svg': 'file','.webm': 'file', '.mid': 'file', '.midi': 'file',
     '.woff': 'file','.woff2': 'file','.ttf': 'file','.otf': 'file','.eot': 'file','.fnt': 'file','.fon': 'file',
