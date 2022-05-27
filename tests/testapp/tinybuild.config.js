ca4f50533c524f6f2bdedbe8707c9eec3183c697
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-05-27 12:38:24 -0700

    edit package boilerplate, push working concurrent server+app boilerplate

--- tests/testapp/tinybuild.config.js
+++ tests/testapp/tinybuild.config.js
@@ -2,7 +2,7 @@
 const config = {
     bundler: { //esbuild settings, set false to skip build step or add bundle:true to config object to only bundle (alt methods)
         entryPoints: [ //entry point file(s). These can include .js, .mjs, .ts, .jsx, .tsx, or other javascript files. Make sure your entry point is a ts file if you want to generate types
-        "index.js"
+            "index.js"
         ],
         outfile: "dist/index", //exit point file, will append .js as well as indicators like .esm.js, .node.js for other build flags
         //outdir:[]               //exit point files, define for multiple bundle files
